bf9889483dbe0dac2c65e6b238a22b3a2a439793
Santiago Suarez <santiago@tryolabs.com>
2024-11-12 11:14:05 -0300

    Clean stuff

--- lorenz/lorenz.py
+++ lorenz/lorenz.py
@@ -1,150 +1,27 @@
-# from manim import *
-# from scipy.integrate import odeint
-# from scipy.integrate import solve_ivp
-
-
-# def lorenz_system(t, state, sigma=10, rho=28, beta=8 / 3):
-#     x, y, z = state
-#     dxdt = sigma * (y - x)
-#     dydt = x * (rho - z) - y
-#     dzdt = x * y - beta * z
-#     return [dxdt, dydt, dzdt]
-
-
-# def ode_solution_points(function, state0, time, dt=0.01):
-#     solution = solve_ivp(
-#         function,
-#         t_span=(0, time),
-#         y0=state0,
-#         t_eval=np.arange(0, time, dt)
-#     )
-#     return solution.y.T
-
-
-# def for_later():
-#     tail = VGroup(
-#         TracingTail(dot, time_traced=3).match_color(dot)
-#         for dot in dots
-#     )
-
-
-# class LorenzAttractor(InteractiveScene):
-#     def construct(self):
-#         # Set up axes
-#         axes = ThreeDAxes(
-#             x_range=(-50, 50, 5),
-#             y_range=(-50, 50, 5),
-#             z_range=(-0, 50, 5),
-#             width=16,
-#             height=16,
-#             depth=8,
-#         )
-#         axes.set_width(FRAME_WIDTH)
-#         axes.center()
-
-#         self.frame.reorient(43, 76, 1, IN, 10)
-#         self.frame.add_updater(lambda m, dt: m.increment_theta(dt * 3 * DEGREES))
-#         self.add(axes)
-
-#         # Add the equations
-#         equations = Tex(
-#             R"""
-#             \begin{aligned}
-#             \frac{\mathrm{d} x}{\mathrm{~d} t} & =\sigma(y-x) \\
-#             \frac{\mathrm{d} y}{\mathrm{~d} t} & =x(\rho-z)-y \\
-#             \frac{\mathrm{d} z}{\mathrm{~d} t} & =x y-\beta z
-#             \end{aligned}
-#             """,
-#             t2c={
-#                 "x": RED,
-#                 "y": GREEN,
-#                 "z": BLUE,
-#             },
-#             font_size=30
-#         )
-#         equations.fix_in_frame()
-#         equations.to_corner(UL)
-#         equations.set_backstroke()
-#         self.play(Write(equations))
-
-#         # Compute a set of solutions
-#         epsilon = 1e-5
-#         evolution_time = 30
-#         n_points = 10
-#         states = [
-#             [10, 10, 10 + n * epsilon]
-#             for n in range(n_points)
-#         ]
-#         colors = color_gradient([BLUE_E, BLUE_A], len(states))
-
-#         curves = VGroup()
-#         for state, color in zip(states, colors):
-#             points = ode_solution_points(lorenz_system, state, evolution_time)
-#             curve = VMobject().set_points_smoothly(axes.c2p(*points.T))
-#             curve.set_stroke(color, 1, opacity=0.25)
-#             curves.add(curve)
-
-#         curves.set_stroke(width=2, opacity=1)
-
-#         # Display dots moving along those trajectories
-#         dots = Group(GlowDot(color=color, radius=0.25) for color in colors)
-
-#         def update_dots(dots, curves=curves):
-#             for dot, curve in zip(dots, curves):
-#                 dot.move_to(curve.get_end())
-
-#         dots.add_updater(update_dots)
-
-#         tail = VGroup(
-#             TracingTail(dot, time_traced=3).match_color(dot)
-#             for dot in dots
-#         )
-
-#         self.add(dots)
-#         self.add(tail)
-#         curves.set_opacity(0)
-#         self.play(
-#             *(
-#                 ShowCreation(curve, rate_func=linear)
-#                 for curve in curves
-#             ),
-#             run_time=evolution_time,
-#         )
-
 import numpy as np
 from manim import (
     BLUE,
-    BLUE_A,
-    BLUE_E,
     DEFAULT_STROKE_WIDTH,
-    DEGREES,
-    GREEN,
-    IN,
     LEFT,
     PI,
     RED,
     RIGHT,
-    TAU,
     UL,
-    UP,
     Circle,
     Create,
     Dot,
-    Group,
-    Rotate,
     Scene,
     Tex,
     ThreeDAxes,
     ThreeDScene,
     TracedPath,
-    UpdateFromAlphaFunc,
     VGroup,
     VMobject,
     Write,
     color_gradient,
     linear,
 )
-from scipy.integrate import odeint, solve_ivp
+from scipy.integrate import solve_ivp
 
 
 def lorenz_system(t, state, sigma=10, rho=28, beta=8 / 3):
@@ -212,7 +89,7 @@ class LorenzAttractor(ThreeDScene):
         # Compute a set of solutions
         epsilon = 1
         evolution_time = 30
-        n_points = 2
+        n_points = 5
         states = [[10, 10, 10 + n * epsilon] for n in range(n_points)]
         colors = color_gradient([BLUE, RED], len(states))
 
@@ -223,7 +100,7 @@ class LorenzAttractor(ThreeDScene):
             # convert to manim object and set style
             curve = VMobject()
             curve.set_points_smoothly([axes.c2p(*point) for point in points])
-            curve.set_stroke(color, 3, opacity=0.9)
+            curve.set_stroke(color, 2, opacity=0.9)
             # add to Vgroup
             curves.add(curve)
             # self.play(Create(curve), run_time=20)
@@ -234,36 +111,6 @@ class LorenzAttractor(ThreeDScene):
             run_time=30,
             rate_func=linear,
         )
-
-        # # Display dots moving along those trajectories
-        # dots = VGroup(*[Dot(color=color, radius=0.25) for color in colors])
-
-        # # Create the traced path (trails)
-        # traced_paths = VGroup(
-        #     *[
-        #         TracedPath(dot.get_center, dissipating_time=3).match_color(dot)
-        #         for dot in dots
-        #     ]
-        # )
-
-        # def update_dots(dots, dt):
-        #     for dot, curve in zip(dots.submobjects, curves):
-        #         alpha = (
-        #             self.time / evolution_time
-        #         ) % 1  # Use time to calculate the position along the curve
-        #         dot.move_to(curve.point_from_proportion(alpha))
-
-        # # Add updater for the dots
-        # dots.add_updater(update_dots)
-        # self.add(dots, traced_paths)
-
-        # # Play the animation over evolution_time
-        # self.play(
-        #     UpdateFromAlphaFunc(dots, update_dots),
-        #     run_time=evolution_time,
-        #     rate_func=linear,
-        # )
-
         # Stop camera rotation at the end
         self.stop_ambient_camera_rotation(about="theta")
 
